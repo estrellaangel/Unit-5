b95a3527578140df2cf8eeef2d8a77dde113c708
Estrella <ella34138@gmail.com>
2021-01-27 15:29:02 -0700

    more js a bit of css but needs more work

--- passengerProject/script.js
+++ passengerProject/script.js
@@ -11,6 +11,12 @@ let numberID = 5;
 let passengerNames = [];
 //List of all passengers, have to refer by elements of the element
 
+let mealList = ["", "Chicken Sandwhich", "Spagehti & Meatballs", "Salmon", "Salad"];
+let extrasList = ["", "Window Seat", "Aisle Seat", "Extra Leg Room", "Headphones", "Extra Food"];
+
+
+
+console.log(new Date())
 class Passenger{
     constructor(first, last, birthday, startingCity, departure, endingCity, returnDate, meal, extras, baggageN){
         this.firstName = first;
@@ -24,6 +30,30 @@ class Passenger{
         this.meal = meal;
         this.extras = extras;
         this.baggageN = baggageN
+    }
+    checkAge(){
+        // // let born = this.birthday;
+        // // let timeAge = Date.now() - born.getTime();
+        // // let timeBetween = new Date(timeAge);
+
+        // // this.age = Math.abs(timeBetween.getUTCFullYear()-1970);
+        // // if(this.age >= 21){
+        // //     this.drink = true;
+        // // }else{
+        // //     this.drink = false;
+        // // }
+        // // console.log(this.drink);
+        // let born = this.birthday;
+        // let month = new Date().getMonth;
+        // let day = new Date().getDate;
+        // let year = new Date().getUTCFullYear;
+
+        let birth = new Date(this.birthday);
+        let now = Date.now()
+        let difference = now - birth.getTime()
+        difference = Math.floor(difference / ( 1000 * 60 * 60 * 24 * 365));
+        this.age = difference;
+        console.log(this.age);
     }
 }
 
@@ -67,7 +97,7 @@ function addPassenger(){
             }
         }
         if(M == 0){
-            alert("Enter Meal Selectin")
+            alert("Enter Meal Selection")
         }
     //This loop finds which meal is selected
 
@@ -83,8 +113,9 @@ function addPassenger(){
     let B = document.getElementById("baggage").value;
     
 
-    var newPassenger = new Passenger(FN, LN, BD, SC, DD, EC, RD, M, E, B);
-
+    let newPassenger = new Passenger(FN, LN, BD, SC, DD, EC, RD, M, E, B);
+    newPassenger.checkAge();
+    console.log(newPassenger);
     passengerNames.push(newPassenger);
 
     document.getElementById(`firstName`).value = "";
@@ -100,7 +131,15 @@ function addPassenger(){
         document.getElementById(`extra${E[i]}`).checked = false;
     };
     numberID++;
-    console.log(passengerNames);
+    showPasInfo(passengerNames, (passengerNames.length -1));
+
+}
+
+function closeBox(){
+    document.getElementById(`specPasInfo`).classList.remove(`specificPassengerSearch`);
+    document.getElementById(`specPasInfo`).classList.add(`hide`);
+    document.getElementById(`editBtn`).classList.add(`hide`);    
+    document.getElementById(`closeBtn`).classList.add(`hide`);
 }
 
 function searchByNameButton(){
@@ -113,16 +152,31 @@ function searchByIdButton(){
 
 function showPasInfo(array, num){
     document.getElementById("specPasInfo").classList.add("specificPassengerSearch");
-    document.getElementById("EditBtn").classList.remove("hide");
+    document.getElementById("specPasInfo").classList.remove("hide");
+    document.getElementById("editBtn").classList.remove("hide");
     document.getElementById("closeBtn").classList.remove("hide");
     document.getElementById(`pname`).innerHTML = `${array[num].firstName} ${array[num].lastName}`;
     document.getElementById('pbirthday').innerHTML = `Birthday: ${array[num].birthday}`;
+    document.getElementById(`page`).innerHTML = `Age: ${array[num].age}`;
+    if(array[num].age >= 21){
+        document.getElementById(`under21`).classList.add(`hide`);
+    }else{
+        document.getElementById(`over21`).classList.add(`hide`);
+    }
     document.getElementById(`pID`).innerHTML = `ID: ${array[num].id}`;
     document.getElementById(`pstartingCity`).innerHTML = `Starting City: ${array[num].startingCity}`;
     document.getElementById(`pstartingDate`).innerHTML = `Leaving Date: ${array[num].departure}`;
     document.getElementById(`preturnDate`).innerHTML = `Returning Date: ${array[num].returnDate}`;
     document.getElementById(`pendingCity`).innerHTML = `Destination: ${array[num].endingCity}`;
     document.getElementById(`pbaggage`).innerHTML = `Baggage Count: ${array[num].baggageN}`;
+    document.getElementById(`pmeal`).innerHTML = `Meal: ${mealList[array[num].meal]}`;
+    let extrasDisplayed = [];
+    for(let i = 1; i<=array[num].extras.length; i++){
+        let add = extrasList[i];
+        extrasDisplayed.push(add);
+    }
+    document.getElementById(`pextras`).innerHTML = `Extras: ${extrasDisplayed}`;
+
 }
 
 function close(){
@@ -142,6 +196,7 @@ function searchList(){
                 for(let i = 0; i<passengerNames.length; i++){
                     if(passengerNames[i].firstName == newArr[0] && passengerNames[i].lastName == newArr[1]){
                     showPasInfo(passengerNames, i);
+                    console.log("hello")
                 }}
             }else{
                 alert(`Incorrect First or Last Name`);
