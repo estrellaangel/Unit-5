69a2b8fbf69185a4f5ddc30f05e8bbefd88bef68
Estrella <ella34138@gmail.com>
2021-02-04 15:28:22 -0700

    vool

---
+++ votingProj/script.js
@@ -0,0 +1,9 @@
+// castPageBtn.addEventListener(`click`, function(){
+//     location.replace('voting.html');
+// })
+
+let hide = function(){
+    castVote.style.display = "none";
+}
+
+castVoteBtn.addEventListener(`click`, hide)
